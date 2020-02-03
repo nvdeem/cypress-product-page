d7ac110763671d52e81b2b279e65290c3d388de4
Nadeem Ahmed <nadeem.ahmed@ao.com>
2020-02-03 16:16:47 +0000

    chore: cleanup and refactoring
    author: [NA]

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -21,40 +21,14 @@ Cypress.Commands.add('playGalleryVideo', () => {
 	cy.get('#mediaGalleryVideo').should('be.visible');
 });
 
-Cypress.Commands.add('getProductLinks', (categoryId, env, clientId) => {
-	return new Promise((resolve, reject) => {
-		const url = `https://catalogueservice.products-${env}.ao.com/api/v1/GetListerPage`;
-		const info = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Catalogue-Service-Api-Key': '249986b4-b19b-4a01-b4c7-f0103aad7d11'
-			},
-			body: JSON.stringify({
-				CatalogueQuery: {
-					CategoryIds: [categoryId],
-					Formatting: {
-						StartIndex: 0,
-						PageSize: 10
-					},
-					ProductStates: ['LiveProduct']
-				},
-				CompanyId: clientId
-			})
-		};
-
-		fetch(url, info)
-			.then(response => {
-				if (response.ok) return response.json();
-				throw new Error(response.statusText);
-			})
-			.then(data => {
-				const products = data.Response.Products.map(p => p.ProductLink);
-				resolve(products);
-			})
-			.catch(err => {
-				console.log(err);
-				reject(err);
-			});
-	});
+Cypress.Commands.add('triggerCarouselNavigation', () => {
+	cy.get('[data-testid=carousel-centre-image]').should('be.visible');
+	cy.get('#mediaGalleryNext').click();
+	cy.get('[data-testid=carousel-centre-image]').should('not.be.visible');
+	cy.get('#mediaGalleryNext').click();
+	cy.get('[data-testid=carousel-right-image]').should('not.be.visible');
+	cy.get('#mediaGalleryPrev')
+		.click()
+		.click();
+	cy.get('[data-testid=carousel-left-image]').should('be.visible');
 });
